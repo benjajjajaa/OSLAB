5ffc4cbc73add3c4310eabff811ffb402ca4f967
benjajjajaa <idk.bnjstin@gmail.com>
2026-01-09 00:58:07 +0000

    os

--- exp3/FCFS.c
+++ exp3/FCFS.c
@@ -2,25 +2,25 @@
 
 int main(void)
 {
-    int bt[20], wt[20], tat[20];
-    int i, n;
-    float wtavg = 0, tatavg = 0;
+    int bt[20], wt[20], tat[20]; //capable of storing 20 intgrs
+    int i, n; //i → loop counter ; n → number of processes
+    float wtavg = 0, tatavg = 0; // initialize to zero, bc auto 
 
     printf("Enter the number of processes (max 20): ");
-    scanf("%d", &n);
+    scanf("%d", &n); //Reads the number of processes and stores it in n
 
     if (n <= 0 || n > 20) {
         printf("Invalid number of processes.\n");
         return 1;
     }
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) { // loops until the number or processes is not met
         printf("Enter Burst Time for Process %d: ", i);
-        scanf("%d", &bt[i]);
+        scanf("%d", &bt[i]); //stores in burst time array
     }
 
-    wt[0] = 0;
-    tat[0] = bt[0];
+    wt[0] = 0; //first process never waits
+    tat[0] = bt[0]; //turn around time is equal to burst time
 
     for (i = 1; i < n; i++) {
         wt[i] = wt[i - 1] + bt[i - 1];
@@ -32,10 +32,15 @@ int main(void)
     printf("\nPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\n");
     for (i = 0; i < n; i++) {
         printf("P%d\t%d\t\t%d\t\t%d\n", i, bt[i], wt[i], tat[i]);
-    }
+    } //stores the output
 
-    printf("\nAverage Waiting Time = %.2f", wtavg / n);
-    printf("\nAverage Turnaround Time = %.2f\n", tatavg / n);
+    printf("\nAverage Waiting Time = %.2f", wtavg / n); //formula for AWT
+    printf("\nAverage Turnaround Time = %.2f\n", tatavg / n); //formula for ATT
 
     return 0;
 }
+
+//Easy to implement
+//No starvation - A process is ready to run, but never gets a chance.
+
+//Poor average waiting time - how long a process sits in the ready queue doing nothing before it gets the CPU.
